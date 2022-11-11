26d3061fd4a5eb72f7934a1362e67aec19959116
Roger Jin <roger@LAP-C02YQ0HAJHD2.local>
2022-11-11 00:22:55 -0500

    updates to bleu

--- code/eval_utils.py
+++ code/eval_utils.py
@@ -15,12 +15,12 @@ except LookupError:
     nltk.download('punkt')
 
 from nltk.tokenize import word_tokenize
-from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
+from nltk.translate.bleu_score import sentence_bleu, corpus_bleu, SmoothingFunction
 
 
 def get_bleu(prediction: str, targets: list):
     """
-    get bleu score for a corpus
+    get bleu score for a sentence
 
     Parameters
     ----------
@@ -40,5 +40,33 @@ def get_bleu(prediction: str, targets: list):
     # added smoothing in case 3 or 4 grams not present
     chencherry = SmoothingFunction()
     bleu = sentence_bleu(t_tokens, p_tokens, smoothing_function=chencherry.method1)
+    return bleu
+
+
+def get_bleu_corpus(prediction: list, targets: list, n_grams=4):
+    """
+    get bleu score for a corpus
+
+    Parameters
+    ----------
+    predictions : 
+        a list of predictions
+    targets : 
+        list of lists of candidates
+    n_grams : int, optional
+        max n-gram size for bleu, default 4
+    
+    Returns
+    -------
+    float
+        BLEU score
+    """
+    p_tokens = [word_tokenize(p) for p in prediction]
+    t_tokens = [[word_tokenize(t)  for t in ts] for ts in targets]
+
+    # added smoothing in case 3 or 4 grams not present
+    chencherry = SmoothingFunction()
+    weights = (1/n_grams, ) * n_grams
+    bleu = corpus_bleu(t_tokens, p_tokens, weights=weights, smoothing_function=chencherry.method1)
     return bleu
 
