5e231ca1e04525340343ceb61918788d5864edc8
Roger Jin <roger@LAP-C02YQ0HAJHD2.local>
2022-11-17 04:05:48 -0500

    transformer encoder model implemented

--- code/data/coco.sh
+++ code/data/coco.sh
@@ -1,11 +1,11 @@
 wget http://images.cocodataset.org/zips/train2017.zip
-wget http://images.cocodataset.org/zips/val2017.zip
-wget http://images.cocodataset.org/zips/test2017.zip
-
 unzip train2017.zip
-unzip val2017.zip
-unzip test2017.zip
-
 rm train2017.zip
+
+wget http://images.cocodataset.org/zips/val2017.zip
+unzip val2017.zip
 rm val2017.zip
-rm test2017.zip
+
+# wget http://images.cocodataset.org/zips/test2017.zip
+# unzip test2017.zip
+# rm test2017.zip
---
+++ code/transformer.py
@@ -0,0 +1,49 @@
+import torch
+import numpy as np
+
+from torch import nn
+
+
+class PositionalEncodingLayer(nn.Module):
+    def __init__(self, embedding_dim: int):
+        super().__init__()
+        self.embedding_dim = embedding_dim
+
+    def forward(self, X: torch.Tensor):
+        _, seq_len, _ = X.shape
+        pos_embedding = torch.zeros((seq_len, self.embedding_dim))
+        
+        # get positional vectors
+        pos = torch.arange(seq_len).tile(self.embedding_dim // 2)
+        pos = pos.reshape(self.embedding_dim // 2, seq_len).T
+
+        # get location in embedding dimension
+        denom = torch.arange(0, self.embedding_dim, 2).tile(seq_len)
+        denom = denom.reshape(seq_len, self.embedding_dim // 2) / self.embedding_dim
+
+        sin_term = torch.sin(pos / 10000 ** denom)
+        cos_term = torch.cos(pos / 10000 ** denom)
+
+        # fill positional embedding tensor
+        pos_embedding[:, np.arange(0, self.embedding_dim, 2)] = sin_term
+        pos_embedding[:, np.arange(0, self.embedding_dim, 2) + 1] = cos_term
+
+        return X + pos_embedding
+
+
+class Encoder(nn.Module):
+    def __init__(self, embedding_dim: int, heads: int, layers: int, inp_seq: int, out_seq: int):
+        super().__init__()
+        self.pos_embedding = PositionalEncodingLayer(embedding_dim)
+        block = nn.TransformerEncoderLayer(embedding_dim, heads)
+        self.encoder = nn.TransformerEncoder(block, layers)
+        self.combine = nn.Conv1d(in_channels=inp_seq, out_channels=out_seq, kernel_size=1)
+    
+    def forward(self, X):
+        if len(input.shape) == 2:
+            X = X.reshape(1, X.shape[0], X.shape[1])
+            
+        X = self.pos_embedding.forward(X)
+        X = self.encoder.forward(X)
+        X = self.combine(X)
+        return X
